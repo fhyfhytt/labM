895cb1b2f6d7cfe31c7991ee6a0d2919db078d68
zhz <zhz@zjimee.com>
2020-03-20 17:06:34 +0800

    物资管理页面

---
+++ src/views/deviceManagement/AssetManagement/SuppliesManage/components/tab/addMoudel.vue
@@ -0,0 +1,104 @@
+<template>
+  <div v-loading="loading" class="dialgAddform1">
+    <p class="bkb" />
+    <el-form ref="addForm" :model="addForm" label-width="120px" :rules="addFormRules" class="formAdd" style="margin-left:-10px;">
+      <el-row class="addtop">
+        <el-col :span="12">
+          <el-form-item label="组织编号:" prop="code">
+            <el-input v-model="addForm.code" placeholder="请输入组织编号" />
+          </el-form-item>
+          <el-form-item label="组织类型:" prop="type">
+            <el-select v-model="addForm.type" popper-class="select-option" placeholder="-请选择-">
+              <el-option v-for="item in selectDataTrue" :key="item.value" :label="item.name" :value="item.value" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="组织名称:" prop="name">
+            <el-input v-model="addForm.name" placeholder="请输入组织名称" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="描述:" prop="description">
+            <el-input v-model="addForm.description" type="textarea" placeholder="请输入备注" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+    <div class="dialog-footer">
+      <el-button size="medium " class="button-sub" @click.native="submitForm('addForm')">确定</el-button>
+      <el-button size="medium " class="button-cancel" @click="handeladdFormVisible">取消</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { managementCenterEditNew, getListSelectNew } from '@/api/userManagement.js'
+export default {
+  props: {
+    treeId: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      // 新增界面数据  state: '',
+      addForm: { code: '', name: '', description: '', type: '', parentId: this.treeId },
+      addFormVisible: false,
+      loading: false,
+      addFormRules: {
+        code: [{ required: true, message: '请输入组织编号', trigger: 'blur' }],
+        name: [{ required: true, message: '请输入组织名称', trigger: 'blur' }],
+        type: [{ required: true, message: '请输入组织类型', trigger: 'blur' }]
+      },
+      selectDataTrue: [{ name: '业务', value: '9cbf144f21044b1e8510f9e607addcbc' }, { name: '管理', value: '8baa547b59504e8cb25424446222ab8d' }]
+    }
+  },
+  methods: {
+    // 取消新增
+    handeladdFormVisible(addFormVisible) {
+      this.$emit('handeladdFormVisible', this.addFormVisible)
+      this.addForm = {}
+    },
+    // 提交新增
+    submitForm(formName) {
+      var that = this
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          getListSelectNew({ code: this.addForm.code }).then(response => {
+            if (response.data.count !== 0) {
+              this.$message.error('编号已存在，不可新增！')
+              return false
+            } else {
+              const param = this.addForm
+              managementCenterEditNew(param).then(response => {
+                that.loading = false
+                if (response.success === true) {
+                  this.$message.success('新增成功')
+                  this.$emit('handleGetTree1')
+                  this.handeladdFormVisible()
+                } else {
+                  this.$message.error(response.msg)
+                }
+              }).catch(response => {
+                this.$message.error(response.message)
+              })
+            }
+          }).catch(response => {
+            this.$message.error(response.message)
+          })
+        } else {
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~@/styles/modul.scss';
+</style>
--- src/views/deviceManagement/AssetManagement/SuppliesManage/view.vue
+++ src/views/deviceManagement/AssetManagement/SuppliesManage/view.vue
@@ -1,13 +1,216 @@
 <template>
-  <div class="page">
-    物资管理
+  <div class="pageRow">
+    <el-row>
+      <!--树结构-->
+      <el-col :xl="{span:3}" :lg="{span:5}">
+        <div class="tree-div">
+          <div class="inline-div">
+            <el-tree ref="tree" v-loading="treeloading" :data="treedata" node-key="id" default-expand-all :props="defaultProps" :highlight-current="true" :expand-on-click-node="false" @node-click="handleNodeClick">
+              <span slot-scope="{ node, data }" class="custom-tree-node span-ellipsis">
+                <span v-if="data.children" :title="data.name">
+                  <i class="iconfont iconzuzhi" />{{ data.name }}
+                </span>
+                <span v-else style="paddingLeft:16px;" :title="data.name">
+                  <i class="iconfont iconbumen" />{{ data.name }}
+                </span>
+              </span>
+            </el-tree>
+          </div>
+        </div>
+      </el-col>
+      <!--表格-->
+      <el-col :xl="{span:21}" :lg="{span:19}" class="right-page">
+        <el-row>
+          <el-col>
+            <div class="right-page-title">
+              <el-form ref="form1" :model="formRegion" size="small" label-width="110px" :inline="true">
+                <el-row>
+                  <el-form-item label="数据域名称：">
+                    <el-input v-model="formRegion.name" style="background:transparent;width:150px;" placeholder="请输入数据域名称" />
+                  </el-form-item>
+                  <el-form-item>
+                    <el-button v-permission="'regionSearch'" size="small" class="button-sub" @click="searchData">查询</el-button>
+                  </el-form-item>
+                </el-row>
+              </el-form>
+            </div>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="24">
+            <div class="right-page-table">
+              <tableManageParent :show="show" :table-loading="tableloading" :table-date="tableData" :tree-id="treeId" :total-num="totalNum" @refresh="getTreeData" @handleGetTableData="handleGetTableData" />
+            </div>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
   </div>
 </template>
-<script>
-export default {
 
+<script>
+import {
+  getOrgTreeNew,
+  getListSelectNew
+} from '@/api/userManagement.js'
+import { setTreeData } from '@/utils/utils.js'
+import tableManageParent from './components/tab/table.vue'
+export default {
+  name: '',
+  components: {
+    tableManageParent
+  },
+  data() {
+    return {
+      selectedCode: '', // 组织编号
+      selectedName: '', // 组织名称
+      selectedDescription: '', // 描述
+      // selectedCodeShow: false, // 组织编号是否显示
+      tableManageParent: true, // 组织管理下父级管理中心是否显示
+      // orgtype: '', // 区分是否是最后一级
+      treedata: [], // tree树
+      currentNodekey: '', // 默认高亮修改
+      expandedkeys: [], // 默认高亮修改
+      id: '', // 获取tree树的id
+      selectTree: {}, // 获取点击tree树的的整个对象数据
+      type: '', // 点击tree传参type
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      },
+      tableData: [],
+      treeName: '', // 上级管理中心名称
+      treeId: '', // 上级管理中心id
+      show: true,
+      totalNum: 0,
+      param: {
+        pageSize: 10,
+        pageNumber: 1
+      },
+      // selectData: {} // 点击tree树获取整个节点对象
+      // isDel: true // 最初默认标识可以删除
+      treeloading: true,
+      tableloading: true
+    }
+  },
+  mounted() {
+    // 获取tree树
+    this.getTreeData()
+  },
+  methods: {
+    // 获取tree树数据
+    getTreeData(value) {
+      if (value) {
+        this.treeloading = value.loading
+      }
+      getOrgTreeNew().then(response => {
+        this.treeloading = false
+        if (response.code === 0) {
+          this.treedata = setTreeData(response.data)
+          if (response.data.length > 0) {
+            if (this.treeId === '') {
+              this.treeId = this.treedata[0].id// 当前的Id
+              this.selectedCode = this.treedata[0].code
+              this.selectedName = this.treedata[0].name
+              this.selectedDescription = this.treedata[0].description
+            }
+            this.param.parentId = this.treeId
+            this.currentNodekey = this.treeId
+            this.expandedkeys.push(this.treeId)
+            this.$nextTick(() => {
+              this.$refs.tree.setCurrentKey(this.currentNodekey)
+            })
+            this.getTableData()
+          }
+        } else {
+          this.$message.error(response.msg)
+        }
+      }).catch(response => {
+        this.loading = false
+        this.$message.error(response.message)
+      })
+    },
+    // 点击tree树获取table表格的数据
+    getTableData() {
+      getListSelectNew(this.param)
+        .then(response => {
+          this.tableloading = false
+          if (response.code === 0) {
+            this.tableData = response.data.cfList
+            this.totalNum = Number(response.data.count)
+          } else {
+            this.tableData = []
+            this.totalNum = 0
+            this.$message.error(response.msg)
+          }
+        })
+        .catch(response => {
+          this.tableData = []
+          this.totalNum = 0
+          this.$message.error(response.msg)
+        })
+    },
+    // handleGetTree() {
+    //   this.getTreeData()
+    // },
+    // 分页掉接口
+    handleGetTableData(value) {
+      if (value) {
+        this.tableloading = value.loading
+        this.pageSize = value.pageSize
+        this.pageNumber = value.pageNumber
+        this.getTableData()
+      }
+    },
+    // 点击tree树
+    handleNodeClick(data) {
+      console.log(data)
+      this.param.parentId = data.id
+
+      this.selectedCode = data.code
+      this.selectedName = data.name
+      this.selectedDescription = data.description
+      this.treeId = data.id
+
+      this.getTableData()
+    }
+  }
 }
 </script>
-<style lang="">
 
+<style lang="scss">
+@import '~@/styles/mixin.scss';
+@import '~@/styles/variables.scss';
+.origanmanage {
+  .title-content {
+    margin: 1%;
+    background-size: 100% 100%;
+    border-radius: 5px;
+    border: 1px solid #50dcf5;
+    height: 50px;
+    color: #fff;
+    padding-top: 12px;
+    font-size: 14px;
+  }
+  .contener {
+    margin-left: 1%;
+    margin-right: 1%;
+    background-size: 100% 100%;
+    border-radius: 5px;
+    border: 1px solid #50dcf5;
+    height: 735px;
+  }
+  // .el-tree .el-tree-node>.el-tree-node__children {
+  //   overflow: none;
+  // }
+}
+</style>
+
+<style lang="scss" scoped>
+  .span-ellipsis {
+    width: 100%;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
 </style>
