fbb24a527c51f6f5bd14eb01e5efb6c7aac674eb
fuhy <fuhy@zjimee.com>
2020-03-24 13:24:35 +0800

    route

--- src/router/modules/manageControl.js
+++ src/router/modules/manageControl.js
@@ -33,13 +33,13 @@ const manageControlRouter = {
       name: 'OrganManage'
     },
     {
-      path: 'customManage',
+      path: 'customerManage',
       component: () =>
         import(
-          '@/views/systemManagement/UserManagement/customManage/view.vue'
+          '@/views/systemManagement/UserManagement/customerManage/view.vue'
         ),
-      meta: { title: '客户管理', icon: 'icon111', roles: ['customManage'] },
-      name: 'customManage'
+      meta: { title: '客户管理', icon: 'icon111', roles: ['customerManage'] },
+      name: 'customerManage'
     },
     {
       path: 'roleManage',
