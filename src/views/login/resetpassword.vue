748ec6bd08de30b477e2f9ab4a524a92553029b4
fuhy <fuhy@zjimee.com>
2020-03-27 16:50:18 +0800

    css

--- src/views/login/resetpassword.vue
+++ src/views/login/resetpassword.vue
@@ -65,7 +65,7 @@ export default {
     var validatePass2 = (rule, value, callback) => {
       if (value === '') {
         callback(new Error('请再次输入新密码'))
-      } else if (value !== this.ruleForm.pass) {
+      } else if (value !== this.ruleForm.newpassword) {
         callback(new Error('两次输入密码不一致!'))
       } else {
         callback()
