2b6879340d46802b052790c3be722a93b1f968f6
fuhy <fuhy@zjimee.com>
2020-03-27 14:59:28 +0800

    css

--- src/views/systemManagement/SystemSet/DictionaryS/EditMoudel.vue
+++ src/views/systemManagement/SystemSet/DictionaryS/EditMoudel.vue
@@ -109,6 +109,7 @@ export default {
 
 <style lang="scss" >
 .DictionarysEdit{
+  margin-right:27px;
    .addtop{
       padding-top:18px;
       margin-left:-13px;
@@ -139,7 +140,6 @@ export default {
 
    .el-textarea.el-input--medium{
       .el-textarea__inner {
-     width:300px;
      margin-left:0px;
      max-height: 70px
    }
